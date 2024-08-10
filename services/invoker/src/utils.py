23dd394e08d18725a37dce0352ccad3007faf82d
Igor Batyukov <igor.batyukov@healsecurity.com>
2024-08-10 22:36:25 +0400

    add NUM_MODELS env var

--- services/invoker/src/utils.py
+++ services/invoker/src/utils.py
@@ -7,9 +7,10 @@ from cachetools import TTLCache
 
 GENERATOR_URL = os.getenv('GENERATOR_URL', 'http://0.0.0.0:8890')
 MODEL_REGISTRY_PATH = os.getenv('MODEL_REGISTRY')
+NUM_MODELS = int(os.getenv('NUM_MODELS', 5))
 
 
-def get_model_names(registry_path: str, num: int = 5) -> list[str]:
+def get_model_names(registry_path: str, num: int) -> list[str]:
     with open(registry_path, 'r') as registry:
         js = json.loads(registry.read())
         model_names = [
@@ -31,7 +32,7 @@ async def call_generator(url: str, model_name: str, viewer_id: int):
 
 @cached(cache=TTLCache(maxsize=3, ttl=10))
 async def recommend(user_id):
-    model_names = get_model_names(MODEL_REGISTRY_PATH)
+    model_names = get_model_names(MODEL_REGISTRY_PATH, NUM_MODELS)
     return await runcascade(user_id, model_names)
 
 
